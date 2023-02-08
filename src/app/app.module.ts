f4e6d829f5a4baa4b0cad94ed4dbd94bf3e59882
Adrian Santiago <adrias.9000@gmail.com>
2023-02-08 16:07:10 -0600

    seis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+
+//impotamos app.module de la carpeta pokemon
+import { PokemonModule } from './pokemon/pokemon.module';
+import { ComponentsModule } from './components/components.module';
+//import { PokemonlistComponent } from './pokemon/pokemonlist/pokemonlist.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //ComponentsModule
+    //PokemonlistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //para hacer penicione http pero tambien hay que importar el metodo
+    HttpClientModule,
+    PokemonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
